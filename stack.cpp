00a60b78523f3db835619b5160571e4135c63927
yash rai <yash2154rai@gmail.com>
2023-12-20 13:02:21 +0530

    add Stack.cpp file

---
+++ stack.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+struct StackNode
+{
+    int data;
+    StackNode *next;
+};
+
+class Stack
+{
+
+public:
+    int size=0;
+
+    StackNode *head;
+    void Push(int value)
+    {
+        StackNode *newnode = new StackNode();
+        newnode->data = value;
+        newnode->next = head;
+        size++;
+        head = newnode;
+    }
+
+    int Peek()
+    {
+        return head->data;
+    }
+
+    int Pop()
+    {
+        StackNode*current = head;
+        int returnvalue  = head->data;
+        head=current->next;
+        size--;
+        delete current;
+        return returnvalue;
+    }
+
+    
+
+    void display()
+    {
+        StackNode *current = head;
+        while (current != nullptr)
+        {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << "\n";
+    }
+};
+
+int main()
+{
+    Stack stack;
+    stack.Push(10);
+    stack.Push(20);
+    stack.Push(30);
+    stack.Push(40);
+    stack.Push(50);
+    stack.Push(60);
+    stack.display();
+    std::cout<<stack.Pop()<<"\n";
+    std::cout<<stack.size<<"\n";
+    
+    stack.display();
+    std::cout<<stack.Peek()<<"\n";
+    stack.display();
+}
